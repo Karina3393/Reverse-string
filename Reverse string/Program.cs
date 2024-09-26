16497f837e229bee306a887626ac9a8502fb1e1e
карина <@DESKTOP-FP9I0KA>
2024-09-26 14:44:07 +0300

    Add project files.

---
+++ Reverse string/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Reverse_string
+{
+    internal class Program;
+
+    class ReverseString
+    {
+
+        public static string Reverse(string input)
+        {
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static void Main(string[] args)
+        {
+
+            string str1 = "stressed";
+            string str2 = "sports";
+            string str3 = "racecar";
+
+
+            Console.WriteLine(Reverse(str1));
+            Console.WriteLine(Reverse(str2));
+            Console.WriteLine(Reverse(str3));
+        }
+    }
+}
